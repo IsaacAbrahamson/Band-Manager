fa91b9b7c076aa1017c270132e3f97c3edd07176
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2020-11-10 18:18:59 -0500

    add view functionality

--- routes/service.js
+++ routes/service.js
@@ -2,12 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 // Render Service Page
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.send('no service specified')
 })
 
-router.get('/:id', (req, res, next) => {
-  res.send(req.params.id)
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  res.render('service', {
+    message: 'Additional details for service: ' + id
+  })
 })
 
 module.exports = router
