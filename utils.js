87200f87fd6b3ef8060991c7f93fc099816788de
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2020-11-12 23:30:39 -0500

    make dates in correct format

--- utils.js
+++ utils.js
@@ -26,8 +26,8 @@ function parseDate(timestamp) {
 // This function returns the time in the correct entry format
 function formatTime(timestamp) {
   let datetime = new Date(Date.parse(timestamp))
-  let date = datetime.getFullYear() + '/' + ((datetime.getMonth() > 8) ? (datetime.getMonth() + 1) : ('0' + (datetime.getMonth() + 1))) + '/' + ((datetime.getDate() > 9) ? datetime.getDate() : ('0' + datetime.getDate()))
-  let time = datetime.toLocaleTimeString().replace(/:\d+ /, ' ')
+  let date = datetime.getFullYear() + '-' + ((datetime.getMonth() > 8) ? (datetime.getMonth() + 1) : ('0' + (datetime.getMonth() + 1))) + '-' + ((datetime.getDate() > 9) ? datetime.getDate() : ('0' + datetime.getDate()))
+  let time = datetime.toLocaleTimeString().replace(/:\d+ /, ' ').slice(0, -3)
   return date + ' ' + time
 }
 
